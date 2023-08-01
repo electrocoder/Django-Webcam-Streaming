d998f2e06318f2c9a0f9442b3f03130b4f39a8f7
seyittymz <60932271+seyittymz@users.noreply.github.com>
2023-08-02 00:46:03 +0300

    monitor.html create

--- DjangoWebcamStreaming/urls.py
+++ DjangoWebcamStreaming/urls.py
@@ -18,9 +18,14 @@ from django.http import StreamingHttpResponse
 from django.urls import path
 
 from camera import VideoCamera, gen
+from  home import views as home_views
 
 urlpatterns = [
-    path('monitor/', lambda r: StreamingHttpResponse(gen(VideoCamera()),
-                                                     content_type='multipart/x-mixed-replace; boundary=frame')),
+    # path('monitor/', lambda r: StreamingHttpResponse(gen(VideoCamera()),
+                                                    #  content_type='multipart/x-mixed-replace; boundary=frame')),
     path('admin/', admin.site.urls),
+    
+    path('monitor/',home_views.video_monitor, name='video_monitor'),
+    path('video_feed/', home_views.video_feed, name='video_feed'),
+    path('capture_photo/', home_views.capture_photo, name='capture_photo'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 import cv2
 import base64
-import io ,json
+import io,json
 import datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
+from django.shortcuts import render
 
 class VideoCamera(object):
     def __init__(self):
@@ -13,7 +14,6 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
 
-
     def detect_faces(self, frame):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
@@ -21,7 +21,7 @@ class VideoCamera(object):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         return frame
 
-   
+
     def get_frame(self):
         success, image = self.video.read()
         if success:
@@ -46,6 +46,43 @@ class VideoCamera(object):
 
     def start_capturing(self):
         self.is_capturing = True
+
+
+def video_feed(request):
+    try:
+        print("çalışıyor")
+        camera = VideoCamera()
+        return StreamingHttpResponse(gen(camera), content_type='multipart/x-mixed-replace; boundary=frame')
+    except GeneratorExit:
+        pass
+
+
+def video_monitor(request):
+    return render(request, 'monitor.html')
+
+
+def capture_photo(request):
+    camera = VideoCamera()
+    camera.start_capturing()
+
+    # Fotoğrafı al ve base64 kodlamasını yap
+    success, image = camera.video.read()
+    if success:
+        image_with_faces = camera.detect_faces(image)
+        ret, jpeg = cv2.imencode('.jpg', image_with_faces)
+        image_base64 = base64.b64encode(jpeg.tobytes()).decode()
+
+        # Dosya adı ve content type belirle
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        filename = f"photo_{current_time}.jpg"
+        content_type = "image/jpeg"
+
+        # HTTP yanıtını hazırla ve fotoğrafı indir
+        response = HttpResponse(jpeg.tobytes(), content_type=content_type)
+        response["Content-Disposition"] = f"attachment; filename={filename}"
+        return response
+
+    return HttpResponse("Fotoğraf çekilemedi.")
 
 def gen(camera):
     while True:
